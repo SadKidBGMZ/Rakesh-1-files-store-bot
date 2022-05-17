16b62dc58e81486bee5172fa1d4b85753ec9b561
SK Rajii࿐ <104976581+SadKidBGMZ@users.noreply.github.com>
2022-05-17 14:58:21 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : <a href='tg://user?id={OWNER_ID}'>𝐓𝐡𝐢𝐬 𝐏𝐞𝐫𝐬𝐨𝐧</a>\n○ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : <code>𝐏𝐲𝐭𝐡𝐨𝐧𝟑</code>\n○ 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : <a href='https://docs.pyrogram.org/'>𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐚𝐬𝐲𝐧𝐜𝐢𝐨 {__version__}</a>\n○ 𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞 : <a href='https://linktr.ee/sk_source_code'>𝐂𝐥𝐢𝐜𝐤 𝐡𝐞𝐫𝐞</a>\n○ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : @HappyKidBGMZ\n○ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 : @BGM_LinkzZ</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("↪️ 𝐂𝐥𝐨𝐬𝐞 ↩️", callback_data = "close")
                     ]
                 ]
             )
